63a976c65b019b8b7d0f083fbfb273e8bdcecff1
Eunseo Kim <qmffkselsk@naver.com>
2020-03-27 21:22:00 +0900

    swExpert_level_2

--- SW Expert 2단계/SW Expert 2단계/SW_1961.cpp
+++ SW Expert 2단계/SW Expert 2단계/SW_1961.cpp
@@ -3,6 +3,20 @@
 #include<vector>
 using namespace std;
 vector<vector<int>>arr;
+vector<vector<int>>copy_arr;
+void cycle(int n) {
+	int c = 0, cnt = n-1,count=0;
+	while (count!=9) {
+		while (c != n) {
+			for (int j = 0; j < n; j++) {
+				copy_arr[j][cnt+count] = arr[c][j];
+			}
+			c++, cnt--;
+		}
+
+		count += 3, cnt = n - 1, c = 0;
+	}
+}
 int main(void)
 {
 	int t_case, n,k,cnt=0;
@@ -10,12 +24,13 @@ int main(void)
 	for (int i = 1; i <= t_case; i++) {
 		cin >> n;
 		arr.assign(n, vector<int>(n));
+		copy_arr.assign(n*3, vector<int>(n));
 		while (cnt!=n) {
 			for (int j = 0; j < n; j++) {
 				cin >> arr[cnt][j];
 			}
 			cnt++;
 		}
-
+		cycle(n);
 	}return 0;
 }
